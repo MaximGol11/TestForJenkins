72bdca6232fd98f878fe579d9d0c5c669dc244ec
MaximGol11 <maxim.golubtsov@yahoo.com>
2022-11-22 23:52:17 +0600

    Initial commit

---
+++ src/test/java/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+
+    @org.junit.jupiter.api.Test
+    void test1() {
+        
+    }
+
+    @org.junit.jupiter.api.Test
+    void test2() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void  test3() {
+
+    }
+}
